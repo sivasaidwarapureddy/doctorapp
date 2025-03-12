cdd43b51e225ce92773e10a7a40e1d34cabfab7c
Siva Sai Dwarapureddy <137069575+sivasaidwarapureddy@users.noreply.github.com>
2025-03-12 22:54:46 +0530

    Initial commit with final code

---
+++ client/src/Data/data.js
@@ -0,0 +1,46 @@
+export const userMenu = [
+  {
+    name: "Home",
+    path: "/",
+    icon: "fa-solid fa-house",
+  },
+  {
+    name: "Appointments",
+    path: "/appointments",
+    icon: "fa-solid fa-list",
+  },
+  {
+    name: "Apply Doctor",
+    path: "/apply-doctor",
+    icon: "fa fa-user-md",
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+    icon: "fa-solid fa-user",
+  },
+];
+
+// admin menu
+export const adminMenu = [
+  {
+    name: "Home",
+    path: "/",
+    icon: "fa-solid fa-house",
+  },
+  {
+    name: "Doctors",
+    path: "/admin/doctors",
+    icon: "fa fa-user-md",
+  },
+  {
+    name: "Users",
+    path: "/admin/users",
+    icon: "fa-solid fa-user",
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+    icon: "fa-solid fa-user",
+  },
+];
---
+++ client/src/pages/Appointments.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Layout from "../components/Layout";
+import { Table, message } from "antd";
+import { render } from "react-dom";
+import moment from "moment";
+
+const Appointments = () => {
+  const [appointments, setAppointments] = useState([]);
+  const token = localStorage.getItem("token");
+  const getAppointments = async () => {
+    try {
+      const res = await axios.get("/api/v1/user/user-appointments", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (res.data.success) {
+        setAppointments(res.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAppointments();
+  }, []);
+
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "_id",
+    },
+    // {
+    //   title: "Name",
+    //   dataIndex: "name",
+    //   render: (text, record) => (
+    //     <span>
+    //       {record.doctorId.firstName}
+    //       {record.doctorId.lastName}
+    //     </span>
+    //   ),
+    // },
+    // {
+    //   title: "Phone",
+    //   dataIndex: "phone",
+    //   render: (text, record) => <span>{record.doctorId.phone} </span>,
+    // },
+
+    {
+      title: "Date & Time",
+      dataIndex: "date",
+      render: (text, record) => (
+        <span>
+          {moment(record.date).format("DD-MM-YYYY")} &nbsp;
+          {moment(record.time).format("HH:mm")}
+        </span>
+      ),
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+  ];
+
+  return (
+    <Layout>
+      <h1>Appointments List</h1>
+      <Table columns={columns} dataSource={appointments} />
+    </Layout>
+  );
+};
+
+export default Appointments;
